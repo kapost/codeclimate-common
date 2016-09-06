5bc514df041b49a62a0802f94b6248a6188f7724
Paul Sadauskas <psadauskas@gmail.com>
2016-09-06 15:41:45 -0600

    Convert to a real repo, so we can PR, etc...

--- 00-install.sh
+++ 00-install.sh
@@ -1,7 +1,7 @@
 # This will download all these files (inluding this one, so remove it)
 # You can also update the local files the same way
 # If you're feeling daring:
-# curl -L https://gist.githubusercontent.com/paul/262d8140fda8479f4851ac78934f94e5/raw/00-install.sh | bash
+# curl -L https://raw.githubusercontent.com/kapost/codeclimate-common/master/00-install.sh | bash
 
-git archive --format=tar --remote=https://gist.github.com/paul/262d8140fda8479f4851ac78934f94e5 HEAD | tar xf -
+git archive --format=tar --remote=https://github.com/kapost/codeclimate-common HEAD | tar xf -
 rm 00-install.sh
