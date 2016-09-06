983c6834c72db82a9acb5fc79473e3ab8e550a04
Paul Sadauskas <psadauskas@gmail.com>
2016-09-06 15:48:55 -0600

    Add shebang to install script

--- 00-install.sh
+++ 00-install.sh
@@ -1,7 +1,4 @@
-# This will download all these files (inluding this one, so remove it)
-# You can also update the local files the same way
-# If you're feeling daring:
-# curl -L https://raw.githubusercontent.com/kapost/codeclimate-common/master/00-install.sh | bash
+#!/bin/sh
 
 git archive --format=tar --remote=https://github.com/kapost/codeclimate-common HEAD | tar xf -
 rm 00-install.sh
