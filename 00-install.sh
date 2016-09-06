5c91c7b63faf8cd167d133e5bbc072c7629b73b8
Paul Sadauskas <psadauskas@gmail.com>
2016-09-06 15:39:10 -0600

    Update install script with gist repo

--- 00-install.sh
+++ 00-install.sh
@@ -1,4 +1,7 @@
 # This will download all these files (inluding this one, so remove it)
+# You can also update the local files the same way
+# If you're feeling daring:
+# curl -L https://gist.githubusercontent.com/paul/262d8140fda8479f4851ac78934f94e5/raw/00-install.sh | bash
 
-git archive --format=tar --remote=<repository URL> HEAD | tar xf -
+git archive --format=tar --remote=https://gist.github.com/paul/262d8140fda8479f4851ac78934f94e5 HEAD | tar xf -
 rm 00-install.sh
